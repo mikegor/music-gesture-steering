49544be8ff56756d6544b51faa4b2095a8b741e2
mikegor <michgor00@gmail.com>
2021-05-15 14:06:49 +0200

    added music player

--- live-recognition.py
+++ live-recognition.py
@@ -1,16 +1,19 @@
 import numpy as np
 import tensorflow as tf
 import cv2
-import sys
-import os
-import time
-
+import pygame
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1200)
 cap.set(4, 600)
 
-
+music_file = 'music/song.mp3'
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(music_file)
+volume = 0.1
+pygame.mixer.music.set_volume(volume)
+pygame.mixer.music.play()
 
 IMG_CLASS_DICT = {
     0:'down',
@@ -35,7 +38,24 @@ while(True):
 
     pred = model.predict(np.array([img]))
     pred_num = np.argmax(pred, axis=1)
-    print(IMG_CLASS_DICT[pred_num[0]])
+    pred_num = pred_num[0]
+
+    if(pred_num == 0):
+        if(volume > 0):
+            volume = volume - 0.1
+        pygame.mixer.music.set_volume(volume)
+        print('vol down')
+    elif(pred_num == 2):
+        pygame.mixer.music.unpause()
+        print('start')
+    elif(pred_num == 3):
+        pygame.mixer.music.pause()
+        print('stop')
+    elif(pred_num == 4):
+        if(volume < 1):
+            volume = volume + 0.1
+        pygame.mixer.music.set_volume(volume)
+        print('vol up')
 
     k = cv2.waitKey(10)
     if k == ord('q'):
